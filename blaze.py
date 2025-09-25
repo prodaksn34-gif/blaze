78eaadff1418f576b317cb170a5ba1783a485470
prodaksn34-gif <prodaksn34@gmail.com>
2025-09-25 18:39:16 +0500

    Update blaze.py

--- blaze.py
+++ blaze.py
@@ -46,9 +46,9 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
     print("Блейз запущен")
-    app.run_polling()
 
 # Запуск Telegram + Flask
 if __name__ == "__main__":
     threading.Thread(target=main).start()
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
